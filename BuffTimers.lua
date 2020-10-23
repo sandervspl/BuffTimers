dcea07ddf83ecbf0a4e198dda3fad20b6e5770ad
Sander Vispoel <sandervispoel@hotmail.com>
2020-10-23 21:19:08 +0200

    Remove slash command code

--- BuffTimers.lua
+++ BuffTimers.lua
@@ -1,27 +1,3 @@
-SLASH_BUFFTIMERS1 = "/bufftimers"
-SLASH_BUFFTIMERS2 = "/bt"
-
-local function commandMessage(msg, msg2)
-    DEFAULT_CHAT_FRAME:AddMessage("|caaaaaaaa" .. msg .. "|r" .. " " .. msg2)
-end
-
-SlashCmdList["BUFFTIMERS"] = function(msg)
-    msg = string.lower(msg)
-
-    if msg == "" then
-        DEFAULT_CHAT_FRAME:AddMessage("These are the commands available to the BuffTimers addon:")
-        commandMessage("seconds", "Toggle to show/hide the seconds in the buff time")
-    elseif msg == "seconds" then
-        BuffTimersOptions["seconds"] = not BuffTimersOptions["seconds"]
-
-        if (BuffTimersOptions["seconds"]) then
-            DEFAULT_CHAT_FRAME:AddMessage("BuffTimers: enabled buffs to display seconds.")
-        else
-            DEFAULT_CHAT_FRAME:AddMessage("BuffTimers: disabled buffs to display seconds.")
-        end
-    end
-end
-
 local function formatTime(time)
     local isSecondsOption = BuffTimersOptions["seconds"]
     local seconds = floor(mod(time, 60))
@@ -52,7 +28,7 @@ local function formatTime(time)
 end
 
 local function onAuraDurationUpdate(aura, time)   
-    local duration = getglobal(aura:GetName().."Duration")
+    local duration = getglobal(aura:GetName() .. "Duration")
     
     if (time) then
         duration:SetText(formatTime(time))
@@ -63,9 +39,9 @@ local function onAuraDurationUpdate(aura, time)
 end
 
 local function onAuraUpdate(auraSlot, index, filter)
-    local auraName = auraSlot..index
+    local auraName = auraSlot .. index
     local aura = getglobal(auraName)
-    local auraDuration = getglobal(auraName.."Duration")
+    local auraDuration = getglobal(auraName .. "Duration")
     
     if not auraDuration then
         return
@@ -85,18 +61,3 @@ hooksecurefunc("AuraButton_Update", onAuraUpdate)
 
 -- Aura duration update event
 hooksecurefunc("AuraButton_UpdateDuration", onAuraDurationUpdate)
-
--- Addon load event
-local frame = CreateFrame("FRAME")
-frame:RegisterEvent("ADDON_LOADED")
-
-function frame:OnEvent(event, arg1)
-    if event == "ADDON_LOADED" and arg1 == "BuffTimers" then
-        if (not BuffTimersOptions) then
-            BuffTimersOptions = {}
-            BuffTimersOptions["seconds"] = true
-        end
-    end
-end
-
-frame:SetScript("OnEvent", frame.OnEvent)
